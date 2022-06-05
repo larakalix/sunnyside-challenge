66d3eb2053060257b5cfc80f6930fbd51dd7875a
Ivan Lara <uki@live.co.uk>
2022-06-04 18:47:55 -0600

    Boilerplate started

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
-  )
-}
+const App = () => {
+    return (
+        <div className="flex items-center justify-center w-full m-h-screen">
+            <div className="flex items-center justify-center w-full h-screen">
+                <h1>Hola mundo!</h1>
+            </div>
+        </div>
+    );
+};
 
-export default App
+export default App;
